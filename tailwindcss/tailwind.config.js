304790752b0354a9b8c534b426d1c40272f77df4
Kipl0 <maalmaja@gmail.com>
2023-10-24 20:27:14 +0200

    add custom font families

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../views/*html"],
-  mode: "jit",
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["../views/*html"],
+    mode: "jit",
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Poppins', 'sans'],
+                'playfair-display': ['Playfair Display', 'serif']
+            },
+        },
+    },
+    plugins: [],
 }
 
