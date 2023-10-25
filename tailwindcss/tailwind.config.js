6f467d169cacfff787c187c680639a5bbf3ff053
Kipl0 <maalmaja@gmail.com>
2023-10-25 16:02:33 +0200

    change sans to poppins in custom styles

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                'sans': ['Poppins', 'sans'],
+                'poppins': ['Poppins', 'sans'],
                 'playfair-display': ['Playfair Display', 'serif']
             },
         },
