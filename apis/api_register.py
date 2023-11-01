b21bb6802a71184e47d1f6ebbc19050e8ada3130
Kipl0 <maalmaja@gmail.com>
2023-11-01 13:38:24 +0100

    add profilpic and banners to register

--- apis/api_register.py
+++ apis/api_register.py
@@ -2,7 +2,7 @@ from bottle import post, response, request, time, template
 import x
 import uuid
 import time
-# import bcrypt
+import bcrypt
 import os
 
 @post("/opret-bruger")
@@ -10,6 +10,7 @@ def _():
     try:
         db = x.db()
 
+        # User data
         # Hent valideret data fra form
         user_first_name = x.validate_first_name()
         user_last_name = x.validate_last_name()
@@ -19,6 +20,56 @@ def _():
         x.validate_confirm_password()
 
         user_id = str(uuid.uuid4()).replace("-","")
+
+
+        # bcrypt bruger en hash-funktion sammen med salt, for at generere en hash-værdi, som gemmes i db
+        #bruges til at gemme adgangskoder sikkert, da du kun sammenligner hash-værdier for at bekræfte, om adgangskoden er korrekt.
+        user_password_input = user_password.encode('utf') #Konverter password til bytes
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(user_password_input, salt)
+
+
+        # Upload af billeder til profil
+        rootdir = "C:/Users/maalm/OneDrive/Dokumenter/kea/2_semester/recipe_catalogue/"
+
+        # Profil billede
+        uploaded_profil_pic = request.files.get("uploaded_profil_pic") 
+        if uploaded_profil_pic != None :
+            name, ext = os.path.splitext(uploaded_profil_pic.filename)
+            if ext == "" : 
+                # Fordi formen bruger enctype, er uploaded_profil_pic ikke "none", men ext er (eller en " " )
+                final_profile_pic = "unknown_user.jpg"
+            else:
+                if ext not in(".jpg", ".jpeg", ".png"):
+                    response.status = 400
+                    print(ext)
+                    return "Venligst vælg et billede med en godkendt filtype"
+                final_profile_pic = str(uuid.uuid4().hex)
+                final_profile_pic = final_profile_pic + ext
+                uploaded_profil_pic.save(f"{rootdir}images/profile_images/{final_profile_pic}")
+                # return "Picture uploaded"
+        else :
+            final_profile_pic = "unknown_user.jpg"
+
+        #Upload banner
+        uploaded_banner = request.files.get("uploaded_banner") #files i formen
+        if uploaded_banner != None :
+            name, ext = os.path.splitext(uploaded_banner.filename)
+            if ext == "" : 
+                final_banner = "default_banner.png"
+            else:
+                if ext not in(".jpg", ".jpeg", ".png"):
+                    response.status = 400
+                    raise Exception("Picture not allowed")
+                final_banner = str(uuid.uuid4().hex)
+                final_banner = final_banner + ext
+                uploaded_banner.save(f"{rootdir}banner/{final_banner}")
+                # return "Picture uploaded"
+        else :
+            final_banner = "default_banner.png"
+
+
+        # Indsæt til db
         user = {
             "user_id" : user_id,
             "user_email" : user_email,
@@ -26,11 +77,11 @@ def _():
             "user_first_name" : user_first_name,
             "user_last_name" : user_last_name,
             "user_birthday" : "TBD",
-            "user_password" : user_password,
+            "user_password" : hashed_password,
             "user_created_at" : int(time.time()),
             "user_active" : 1,
-            "user_profilepic" : "TBD",
-            "user_banner" : "TBD",
+            "user_profilepic" : final_profile_pic,
+            "user_banner" : final_banner,
             "user_total_followers" : 0,
             "user_total_following" : 0,
             "user_total_recipes" : 0,
