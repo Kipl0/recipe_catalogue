a001e4320f2e3d034c7319e3dad89ebf8de03aea
Kipl0 <maalmaja@gmail.com>
2023-11-02 12:37:55 +0100

    redirect user from login if user cookie exists

--- routes/login.py
+++ routes/login.py
@@ -1,10 +1,18 @@
-from bottle import get, template
+from bottle import get, request, response, template
 import x
 
 @get("/login")
 def _():
     try:
         db = x.db()
+
+        # Brugeren skal ikke være på login siden, hvis brugeren er logget ind allerede
+        user_cookie = request.get_cookie("user_cookie", secret=x.COOKIE_SECRET)
+
+        if user_cookie:
+            response.status = 303 #fordi 303 bruges til redirecting
+            response.set_header("Location", "/")
+            return
 
         return template("login", title="Login")
 
