ddea53f5282c9729baaa15907b9739b03fc82503
Kipl0 <maalmaja@gmail.com>
2023-11-04 00:27:24 +0100

    fix navbar onclick and add profile dropdown

--- js/toggle_navbar_mobile.js
+++ js/toggle_navbar_mobile.js
@@ -3,17 +3,40 @@
 function toggle_navbar() {
     const bars = document.getElementById("toggle_navbar_bars")    
     const navbar_ul = document.getElementById("navbar_ul")
-
-    if(navbar_ul.classList.contains("hidden")) {
-        navbar_ul.classList.add("flex")
-        navbar_ul.classList.remove("hidden")
-
-        bars.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M18.36 19.78L12 13.41l-6.36 6.37l-1.42-1.42L10.59 12L4.22 5.64l1.42-1.42L12 10.59l6.36-6.36l1.41 1.41L13.41 12l6.36 6.36z"/></svg>'
+    
+    bars.addEventListener("click", function() {
         
-    } else {
-        navbar_ul.classList.add("hidden")
-        navbar_ul.classList.remove("flex")
-        
-        bars.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 14 14"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M13.5 2H.5m13 5H.5m13 5H.5"/></svg>'
-    }
+        if(navbar_ul.classList.contains("hidden")) {
+            navbar_ul.classList.add("flex")
+            navbar_ul.classList.remove("hidden")
+            
+            bars.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M18.36 19.78L12 13.41l-6.36 6.37l-1.42-1.42L10.59 12L4.22 5.64l1.42-1.42L12 10.59l6.36-6.36l1.41 1.41L13.41 12l6.36 6.36z"/></svg>'
+            
+        } else {
+            navbar_ul.classList.add("hidden")
+            navbar_ul.classList.remove("flex")
+            
+            bars.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 14 14"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M13.5 2H.5m13 5H.5m13 5H.5"/></svg>'
+        }
+    })
 }
+
+toggle_navbar()
+
+function toggle_profile_dropdown() {
+    const profile_btn = document.getElementById("profile_btn")
+    const profile_dropdown = document.getElementById("profile_dropdown")
+    
+    profile_btn.addEventListener("click", function() {
+        if(profile_dropdown.classList.contains("hidden")) {
+            profile_dropdown.classList.remove("hidden")
+            profile_dropdown.classList.add("flex")
+        }
+        else {
+            profile_dropdown.classList.remove("flex")
+            profile_dropdown.classList.add("hidden")
+        }
+    })
+}
+
+toggle_profile_dropdown()
