99620e8d3f2f5e248e1de0ec2ffe0d7972d8c93e
Kipl0 <maalmaja@gmail.com>
2023-11-30 21:24:35 +0100

    follow user

---
+++ apis/api_follow.py
@@ -0,0 +1,73 @@
+from bottle import post, response, request
+import x
+
+
+@post("/follow-user")
+def _():
+    try:
+        user_cookie = request.get_cookie("user_cookie", secret=x.COOKIE_SECRET)
+        if user_cookie is not None:
+            user_cookie = x.validate_user_jwt(user_cookie)
+        else:
+            print("Ingen bruger er logget ind.")
+
+        if user_cookie is None:
+            return {"info": "Du skal logge ind for at kunne følge brugere"}
+
+        db = x.db()
+        user_id = request.forms.get("user_id")
+
+        follower_following_record = db.execute("SELECT * FROM follower_following WHERE ff_follower_fk = ? AND ff_following_fk = ?", (user_cookie["user_id"], user_id)).fetchone()  # noqa
+
+        user_followed = db.execute("SELECT * FROM users WHERE user_id = ?", (user_id, )).fetchone()  # noqa
+        user_total_followers = user_followed['user_total_followers']
+
+        # Hvis den er 0, så har de ikke liket opskriften
+        if follower_following_record is None:
+            db.execute("INSERT INTO follower_following VALUES(?,?)",(user_cookie["user_id"], user_id))  # noqa
+            db.commit()
+
+            # logget ind bruger får flere following
+            db.execute("UPDATE users SET user_total_following = user_total_following + 1 WHERE user_id = ?", (user_cookie['user_id'], ))  # noqa
+            db.commit()
+
+            # Den anden bruger får flere followers
+            db.execute("UPDATE users SET user_total_followers = user_total_followers + 1 WHERE user_id = ?", (user_id, ))  # noqa
+            db.commit()
+
+            return {
+                "info": "ok",
+                "user_id": user_id,
+                "user_total_followers": int(user_total_followers) + 1
+            } 
+
+        db.execute("DELETE FROM follower_following WHERE ff_follower_fk = ? AND ff_following_fk = ?", (user_cookie['user_id'], user_id))  # noqa
+        db.commit()
+
+        # logget ind bruger får færre following
+        db.execute("UPDATE users SET user_total_following = user_total_following - 1 WHERE user_id = ?", (user_cookie['user_id'], ))  # noqa
+        db.commit()
+
+        # Den anden bruger får færre followers
+        db.execute("UPDATE users SET user_total_followers = user_total_followers - 1 WHERE user_id = ?", (user_id, ))  # noqa
+        db.commit()
+
+        return {
+            "info": "ok",
+            "user_id": user_id,
+            "user_total_followers": int(user_total_followers) - 1
+        }
+
+    except Exception as ex:
+        try:
+            response.status = ex.args[0]
+            return {"info": ex.args[1]}
+
+        except Exception as ex:
+            response.status = 500
+            print(ex)
+            return {"info": str(ex)}
+
+    finally:
+        if "db" in locals():
+            db.close()
---
+++ js/follow_user.js
@@ -0,0 +1,45 @@
+const followForms = document.querySelectorAll('.follow-form')
+
+// Add a submit event listener to each form
+followForms.forEach(function (form) {
+    form.addEventListener('submit', async function (event) {
+        event.preventDefault()
+
+        const form_id_split = form.id.split("form_follow_user_")
+        const recipe_id = form_id_split[1]
+
+        try {
+            const frm = form
+            const conn = await fetch("/follow-user", {
+                method: "POST",
+                body: new FormData(frm)
+            })
+    
+            //hent respons fra API
+            const data = await conn.json()
+            if(!conn.ok) {
+                console.log("Kan ikke følge bruger")
+                return
+            }
+    
+            // Success
+            if(conn.ok && data.info == "ok") {
+                const unsolid_hrt = document.getElementById(`unsolid_hrt_${recipe_id}`)
+                const solid_hrt = document.getElementById(`solid_hrt_${recipe_id}`)
+
+                if (unsolid_hrt.classList.contains("hidden")){
+                    unsolid_hrt.classList.remove("hidden")
+                    solid_hrt.classList.add("hidden")
+                }
+                else {
+                    solid_hrt.classList.remove("hidden")
+                    unsolid_hrt.classList.add("hidden")
+                }
+            }
+
+        } catch ({ name, message }) {
+            console.log(name)
+            console.log(message)
+        }        
+    })
+})
