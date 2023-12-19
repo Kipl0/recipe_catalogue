6226e093399dce187925ece6a36aff0b652cb3b5
Kipl0 <maalmaja@gmail.com>
2023-12-19 19:51:30 +0100

    Fix missing database line of code

--- x.py
+++ x.py
@@ -34,8 +34,7 @@ def dict_factory(cursor, row):
 def db():
     try:
         # db = sqlite3.connect(str(pathlib.Path(__file__).parent.resolve())+"/recipe.db")  # noqa
-        # db = sqlite3.connect("recipe.db")
-        # db.execute("PRAGMA foreign_keys=ON;")
+        db = sqlite3.connect("recipe.db")
         db.row_factory = dict_factory
         return db
     except Exception as ex:
